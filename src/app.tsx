79ff0d37242756c704e2a30dbea61f90cf94cf4e
Victtor777 <victtor.eng.software@gmail.com>
2024-09-12 15:35:36 -0300

    Finalizando o componente de metas vazias, e separando os componentes

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,15 @@
-import logo from "./assets/logo-in-orbit.svg"
-import letsStart from "./assets/lets-start-illustration.svg"
-import { Plus } from "lucide-react"
-import { Button } from "./components/ui/button"
+import { Dialog } from "./components/ui/dialog"
+import { CreateGoal } from "./components/create-goal"
+import { Summary } from "./components/summary"
+// import { EmptyGoals } from "./components/empty-goals"
 
 export function App() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={logo} alt="logo inorbit" />
-      <img src={letsStart} alt="" />
-      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
-      </p>
+    <Dialog>
+      {/* <EmptyGoals /> */}
+      <Summary />
 
-      <Button>
-        <Plus className="size-4" />
-        Cadastrar Meta
-      </Button>
-    </div>
+      <CreateGoal />
+    </Dialog>
   )
 }
